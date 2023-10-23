038003dfd4c2b65eba1c995ef0aaee7d16cd1a89
Roudi22 <roudi.alzkemi@gmail.com>
2023-10-24 01:09:35 +0200

    some fixes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] text-slate-100">
-      <h1>Portdolio Website</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <HeroSection/>
     </main>
   )
 }
