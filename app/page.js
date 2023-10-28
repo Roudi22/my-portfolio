428861a41b61452fd387fa2324ef47633b58379c
Roudi22 <roudi.alzkemi@gmail.com>
2023-10-28 13:18:55 +0200

    added a div

--- app/page.js
+++ app/page.js
@@ -3,8 +3,10 @@ import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] ">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className='container mx-auto py-4 px-12'>
       <HeroSection/>
+      </div>
     </main>
   )
 }
